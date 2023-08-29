b7e78828c707a550c4696b15c3ec5d51497d6c27
Victor Pena <13victor.pena@gmail.com>
2023-08-29 15:57:54 -0500

    introduced char and int variables

--- new.c
+++ new.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 int main() {
    printf("Hello, World!\n");
-   printf("|\\ \n");
+
+   printf("|\\\n");
    printf("| \\\n");
    printf("|  \\\n");
    printf("|___\\\n");
+
+
+   char characterName[] = "John";
+   int characterAge = 35;
+   printf("there once was a man named %s\n", characterName);
+   printf("he was only %s years old", characterAge);
+   printf("he really liked the name %s\n", characterName);
+   printf("but did not like being %s.\n" characterAge);
+
+
    return 0;
 }
