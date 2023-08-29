34ee94ec2c1e2dfbae95e76095e5dda46e7428ea
Victor Pena <13victor.pena@gmail.com>
2023-08-29 15:58:40 -0500

    update

--- new.c
+++ new.c
@@ -11,10 +11,9 @@ int main() {
    char characterName[] = "John";
    int characterAge = 35;
    printf("there once was a man named %s\n", characterName);
-   printf("he was only %s years old", characterAge);
+   printf("he was only %d years old", characterAge);
    printf("he really liked the name %s\n", characterName);
-   printf("but did not like being %s.\n" characterAge);
-
+   printf("but did not like being %d.\n" characterAge);
 
    return 0;
 }
