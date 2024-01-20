a2e65834ee45176e82dbbae3ce55d419062a6512
Victor Pena <13victor.pena@gmail.com>
2024-01-19 19:32:12 -0600

    format specifiers

--- variables.c
+++ variables.c
@@ -5,5 +5,12 @@ int main() {
 
     char letter = 'a';
 
-    printf("%d", myNumber);
+    float floatingNumber = 15.5;
+
+    printf("%d\n", myNumber);
+    printf("%c\n", letter);
+    printf("%f\n", floatingNumber);
+
+    printf("the letter stored is %c, the number is %d", letter,myNumber);
+
 }
